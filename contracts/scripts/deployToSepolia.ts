1524ffa2048aaab54f4daedcded724d7720d1313
FlemingJohn <flemjohn08@gmail.com>
2026-07-11 14:34:26 +0530

    contracts: deploy treasury safe module

--- contracts/scripts/deployToSepolia.ts
+++ contracts/scripts/deployToSepolia.ts
@@ -12,11 +12,17 @@ async function deployConfidentialTreasuryStack() {
   const intentNetwork = await intentNetworkFactory.deploy(disclosureAuthorityAddress);
   await intentNetwork.waitForDeployment();
 
+  const safeModuleFactory = await ethers.getContractFactory('TreasurySafeModule');
+  const safeModule = await safeModuleFactory.deploy(deployer.address);
+  await safeModule.waitForDeployment();
+
   const disclosureRegistryAddress = await disclosureRegistry.getAddress();
   const intentNetworkAddress = await intentNetwork.getAddress();
+  const safeModuleAddress = await safeModule.getAddress();
 
   console.log(`DisclosureRegistry deployed at ${disclosureRegistryAddress}`);
   console.log(`ConfidentialIntentNetwork deployed at ${intentNetworkAddress}`);
+  console.log(`TreasurySafeModule deployed at ${safeModuleAddress}`);
 }
 
 deployConfidentialTreasuryStack().catch((deploymentError) => {
