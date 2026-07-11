00e5bf4ce3f416b8b904caca21c3ef1e06828c95
FlemingJohn <flemjohn08@gmail.com>
2026-07-11 11:10:31 +0530

    contracts: add hardhat configuration

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const sepoliaRpcUrl = process.env.SEPOLIA_RPC_URL ?? 'https://rpc.sepolia.org';
+const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ?? '';
+
+const hardhatConfiguration: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.27',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: sepoliaRpcUrl,
+      accounts: deployerPrivateKey ? [deployerPrivateKey] : [],
+    },
+  },
+};
+
+export default hardhatConfiguration;
