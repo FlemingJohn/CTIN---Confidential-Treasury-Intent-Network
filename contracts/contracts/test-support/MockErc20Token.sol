c7f0211c219790aa0eaa8529c53f73c92f44d598
FlemingJohn <flemjohn08@gmail.com>
2026-07-18 13:30:33 +0530

    contracts: add approve and transferFrom to mock token

--- contracts/contracts/test-support/MockErc20Token.sol
+++ contracts/contracts/test-support/MockErc20Token.sol
@@ -5,15 +5,31 @@ import {ITransferableToken} from "../interfaces/ITransferableToken.sol";
 
 contract MockErc20Token is ITransferableToken {
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     function mint(address account, uint256 amount) external {
         balanceOf[account] += amount;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        return true;
     }
 
     function transfer(address recipient, uint256 amount) external returns (bool) {
         require(balanceOf[msg.sender] >= amount, "insufficient balance");
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(balanceOf[from] >= amount, "insufficient balance");
+        uint256 approved = allowance[from][msg.sender];
+        require(approved >= amount, "insufficient allowance");
+        allowance[from][msg.sender] = approved - amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
         return true;
     }
 }
