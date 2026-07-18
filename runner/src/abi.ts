83c272c9a136334de2543d9fbcdf49b9cfae97d3
FlemingJohn <flemjohn08@gmail.com>
2026-07-18 13:54:15 +0530

    runner: add intent network abi

---
+++ runner/src/abi.ts
@@ -0,0 +1,78 @@
+export const intentNetworkAbi = [
+  {
+    type: 'function',
+    name: 'openBatch',
+    stateMutability: 'nonpayable',
+    inputs: [],
+    outputs: [{ name: 'batchId', type: 'uint256' }],
+  },
+  {
+    type: 'function',
+    name: 'closeBatch',
+    stateMutability: 'nonpayable',
+    inputs: [{ name: 'batchId', type: 'uint256' }],
+    outputs: [],
+  },
+  {
+    type: 'function',
+    name: 'settleBatch',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'batchId', type: 'uint256' },
+      { name: 'settlementReference', type: 'bytes32' },
+    ],
+    outputs: [],
+  },
+  {
+    type: 'function',
+    name: 'executeSettlement',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'batchId', type: 'uint256' },
+      { name: 'safe', type: 'address' },
+      { name: 'assetIn', type: 'address' },
+      { name: 'assetOut', type: 'address' },
+      { name: 'netAmountIn', type: 'uint256' },
+      { name: 'minimumAmountOut', type: 'uint256' },
+      { name: 'recipient', type: 'address' },
+      { name: 'settlementReference', type: 'bytes32' },
+    ],
+    outputs: [{ name: 'amountOut', type: 'uint256' }],
+  },
+  {
+    type: 'function',
+    name: 'batchStatusOf',
+    stateMutability: 'view',
+    inputs: [{ name: 'batchId', type: 'uint256' }],
+    outputs: [{ name: '', type: 'uint8' }],
+  },
+  {
+    type: 'function',
+    name: 'encryptedBuyTotalOf',
+    stateMutability: 'view',
+    inputs: [{ name: 'batchId', type: 'uint256' }],
+    outputs: [{ name: '', type: 'bytes32' }],
+  },
+  {
+    type: 'function',
+    name: 'encryptedSellTotalOf',
+    stateMutability: 'view',
+    inputs: [{ name: 'batchId', type: 'uint256' }],
+    outputs: [{ name: '', type: 'bytes32' }],
+  },
+  {
+    type: 'function',
+    name: 'intentCountOf',
+    stateMutability: 'view',
+    inputs: [{ name: 'batchId', type: 'uint256' }],
+    outputs: [{ name: '', type: 'uint256' }],
+  },
+  {
+    type: 'event',
+    name: 'BatchOpened',
+    inputs: [
+      { name: 'batchId', type: 'uint256', indexed: true },
+      { name: 'openedAtTimestamp', type: 'uint256', indexed: false },
+    ],
+  },
+] as const;
